9683a3f88b22f32a3c848790444c3b8730f1d171
Anthony Keelan <anthony.keelan@gmail.com>
2017-03-25 23:53:25 -0600

    Swift 3 Version App

---
+++ Roastme/Roastme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Roastme
+//
+//  Created by Anthony Keelan on 2017-03-25.
+//  Copyright © 2017 OneDayApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
