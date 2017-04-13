8f2bb0520d60fcec43ac99fca12ec7cede66b26b
Matthew Belley <MattBelley@Anthonys-Mac.local>
2017-04-12 19:05:19 -0700

    Imagepicker now places chosen image in the imagepreview

--- Roastme/Roastme/CreateRoastController.swift
+++ Roastme/Roastme/CreateRoastController.swift
@@ -11,7 +11,7 @@ import UIKit
 
 class CreateRoastController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
 {
-    
+    @IBOutlet var previewImage: UIImageView!
     
     @IBAction func uploadRoastPicture(_ sender: UIButton) {
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
@@ -20,13 +20,26 @@ class CreateRoastController: UIViewController, UIImagePickerControllerDelegate, 
             imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
             imagePicker.allowsEditing = false
             self.present(imagePicker, animated: true, completion: nil)
+            
+            //previewImage.image = [UIImagePickerControllerOriginalImage] as UIImage
+            
         }
     }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        previewImage.image = chosenImage
+        dismiss(animated: true, completion: nil)
+    }
     
-    override func viewDidLoad() {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+        override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
     
     
     
