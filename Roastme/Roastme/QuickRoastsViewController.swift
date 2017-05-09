cccccffd7fe8483bf8980da1d13e31ba921488cd
Matthew Belley <MattBelley@Anthonys-Mac.local>
2017-05-08 21:36:05 -0700

    handle some edge cases where roasts return nil

--- Roastme/Roastme/QuickRoastsViewController.swift
+++ Roastme/Roastme/QuickRoastsViewController.swift
@@ -10,6 +10,7 @@ import Foundation
 import UIKit
 
 // need to add caching for roasts to eliminate latency ie, store roast data in an array of 3 roasts [prev, current, next]
+// need handling for the case where a roast ID does not exist in the DB
 
 class QuickRoastsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -17,24 +18,36 @@ class QuickRoastsViewController: UIViewController, UITableViewDataSource, UITabl
     @IBOutlet weak var roastCaption: UILabel!
     @IBOutlet weak var roastComments: UITableView!
     
+    var loadTableFlag : Int = 0
     var numCommentRows : Int?
     var userComments : [Comment] = []
-    var roastID:Int = 6 //make random, code on line below may work once Anto pushes roastCount from the server
+    var roastID:Int = 1 //make random, code on line below may work once Anto pushes roastCount from the server
     // var roastID:Int = arc4random_uniform(roastCount) //make random later
-    let firstRoast:Int = 6 //get from DB
+    let firstRoast:Int = 1 //get from DB
     
     override func viewDidLoad() {
         super.viewDidLoad()
       
-        loadRoast()
-        loadComments()
+        RoastAPI.getRoastCount(callback: {
+            (count: Int?) in
+            if count != 0 {
+                self.loadRoast()
+                self.loadComments()
+                self.loadTableFlag = 1
+            }
+            // Do something about nil
+        })
+        
+        
         
         //setup UITableView delegates
-      self.roastComments.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+
+        self.roastComments.register(commentCell.self, forCellReuseIdentifier: "Cell")
         roastComments.delegate = self
         roastComments.dataSource = self
-        
+ 
         //Guesture recognizers
+        if loadTableFlag == 1 {
         let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeRightActions))
         swipeRight.direction = .right
         self.view.addGestureRecognizer(swipeRight)
@@ -42,6 +55,7 @@ class QuickRoastsViewController: UIViewController, UITableViewDataSource, UITabl
         let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeLeftActions))
         swipeLeft.direction = .left
         self.view.addGestureRecognizer(swipeLeft)
+        }
     }
     
     @IBAction func createComment(_ sender: Any) {
@@ -133,11 +147,12 @@ class QuickRoastsViewController: UIViewController, UITableViewDataSource, UITabl
             (comments:[Comment]?) in
             if comments != nil {
                 //populate table view
+                self.loadTableFlag = 1
                 self.numCommentRows = comments?.count
                 self.userComments = comments!
                 self.roastComments.reloadData()
             } else{
-                
+                self.loadTableFlag = 0
             }
         })
     }
@@ -155,15 +170,17 @@ class QuickRoastsViewController: UIViewController, UITableViewDataSource, UITabl
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! commentCell
         print("loading tableview")
-      
+
+        if loadTableFlag != 0 {
         let commentText = self.userComments[indexPath.row].content
         let saltValue = String(describing: self.userComments[indexPath.row].salt)
         let sauceValue = String(describing: self.userComments[indexPath.row].sauce)
-      
-        cell.comment.text = commentText
-        cell.salt.text = saltValue
-        cell.sauce.text = sauceValue
         
+        cell.comment?.text = commentText
+        cell.salt?.text = saltValue
+        cell.sauce?.text = sauceValue
+            
+        }
         return cell
     }
     
@@ -177,7 +194,6 @@ class QuickRoastsViewController: UIViewController, UITableViewDataSource, UITabl
 class commentCell: UITableViewCell {
   @IBOutlet weak var comment: UILabel!
   @IBOutlet weak var sauce: UILabel!
-
   @IBOutlet weak var salt: UILabel!
   
 }
